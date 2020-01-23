e2e54b744d0011f0280a23b2b493436fe26f86e3
David Mills <d.e.mills@stats4sd.org>
2020-01-23 15:44:00 +0000

    Updating database script;  - adding new tables for samples and survey data  - seperating insert statements into seperate script files

---
+++ Mysql/insert_test_forms.sql
@@ -0,0 +1,5 @@
+-- Dumping data for table ukraine.forms: ~1 rows (approximately)
+/*!40000 ALTER TABLE `forms` DISABLE KEYS */;
+INSERT INTO `forms` (`id`, `title`, `xform_id`) VALUES
+    ('agRjX9kiP7TSU45B8aLsUN', 'Iodine Survey - Building Listing', 'ukraine_iodine_building_listing');
+/*!40000 ALTER TABLE `forms` ENABLE KEYS */;
