bda19c4457dea885ad990a603873c8b257663538
David Mills <d.e.mills@stats4sd.org>
2020-02-12 14:31:43 +0000

    updating clusters counts view to add cluster_completed variable

--- Mysql/clusters_with_dwelling_counts.sql
+++ Mysql/clusters_with_dwelling_counts.sql
@@ -1,31 +1,35 @@
 SELECT
-  clusters.id as id,
-  clusters.region_id as region_id,
-  regions.name_en as region_name_en,
-  regions.name_uk as region_name_uk,
-  clusters.sample_id,
-  clusters.type,
-  clusters.locality_type,
-  clusters.num_voters,
-  clusters.smd_id,
-  buildings_per_cluster.tot as tot_buildings,
-  sum(dwellings_per_building.tot) as tot_dwellings
-  FROM clusters
-  LEFT JOIN regions on regions.id = clusters.region_id
-  JOIN (
-    SELECT cluster_id, count(id) as tot
-    FROM buildings
-    GROUP BY cluster_id
-  ) as buildings_per_cluster
-  on buildings_per_cluster.cluster_id = clusters.id
-  JOIN (
-    SELECT
-    dwellings.building_id,
-    count(dwellings.id) as tot,
-    buildings.cluster_id
-    FROM dwellings
-    LEFT JOIN buildings on buildings.id = dwellings.building_id
-    GROUP BY building_id
-  ) as dwellings_per_building
-  on dwellings_per_building.cluster_id = clusters.id
-  GROUP BY clusters.id;
+    `clusters`.`id` AS `id`,
+    `clusters`.`region_id` AS `region_id`,
+    `regions`.`name_en` AS `region_name_en`,
+    `regions`.`name_uk` AS `region_name_uk`,
+    `clusters`.`sample_id` AS `sample_id`,
+    `clusters`.`type` AS `type`,
+    `clusters`.`locality_type` AS `locality_type`,
+    `clusters`.`num_voters` AS `num_voters`,
+    `clusters`.`smd_id` AS `smd_id`,
+    `buildings_per_cluster`.`tot` AS `tot_buildings`,
+    sum(`dwellings_per_building`.`tot`) AS `tot_dwellings`,
+    if((sum(`dwellings_per_building`.`tot_collected`) = 16), 1, 0) AS `cluster_completed`
+    FROM (((`clusters`
+    LEFT JOIN `regions` ON ((`regions`.`id` = `clusters`.`region_id`)))
+JOIN (
+SELECT
+    `buildings`.`cluster_id` AS `cluster_id`,
+    count(`buildings`.`id`) AS `tot`
+FROM
+    `buildings`
+GROUP BY
+    `buildings`.`cluster_id`) `buildings_per_cluster` ON ((`buildings_per_cluster`.`cluster_id` = `clusters`.`id`)))
+JOIN (
+SELECT
+    `dwellings`.`building_id` AS `building_id`,
+    sum(`dwellings`.`data_collected`) AS `tot_collected`,
+    count(`dwellings`.`id`) AS `tot`,
+    `buildings`.`cluster_id` AS `cluster_id`
+FROM (`dwellings`
+    LEFT JOIN `buildings` ON ((`buildings`.`id` = `dwellings`.`building_id`)))
+GROUP BY
+    `dwellings`.`building_id`) `dwellings_per_building` ON ((`dwellings_per_building`.`cluster_id` = `clusters`.`id`)))
+GROUP BY
+    `clusters`.`id`
