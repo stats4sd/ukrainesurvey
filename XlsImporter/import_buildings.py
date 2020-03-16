a6284a3df61477ff95687a2b5c88b868bd84be91
David Mills <d.e.mills@stats4sd.org>
2020-03-16 21:29:20 +0000

    updates to buildings importer:  - wrapping main process in function to allow for importing  - fix for issue where no submissions were present in db

--- XlsImporter/import_buildings.py
+++ XlsImporter/import_buildings.py
@@ -5,8 +5,7 @@ import mysql.connector
 import os
 from datetime import datetime, timedelta
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-wd = os.path.dirname(dir_path)
+
 
 #####################################
 # Method to convert Kobo's date-time strings into a MySQL-friendly date-time string.
@@ -26,143 +25,153 @@ def to_mysql_date_time(string):
 # Get Config Details
 #####################################
 
-with open(os.path.join(wd, 'config.yml')) as config_file:
-    config = yaml.full_load(config_file);
+def import_buildings():
 
-kobo_user = config['kobo']['user']
-kobo_pass = config['kobo']['password']
-sql_user = config['mysql']['user']
-sql_pass = config['mysql']['password']
-sql_db = config['mysql']['db']
-sql_host = config['mysql']['host']
-form_uid = config['kobo']['building_form_uid']
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    wd = os.path.dirname(dir_path)
 
-data_url = 'https://kobo.humanitarianresponse.info/api/v2/assets'
-headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+    print(dir_path)
 
-#####################################
-# GET most recent submission_date from db
-#####################################
-mydb = mysql.connector.connect(
-                                   host=sql_host,
-                                   user=sql_user,
-                                   passwd=sql_pass,
-                                   database=sql_db
-                                   )
+    with open(os.path.join(wd, 'config.yml')) as config_file:
+        config = yaml.full_load(config_file);
 
-mycursor = mydb.cursor()
+    kobo_user = config['kobo']['user']
+    kobo_pass = config['kobo']['password']
+    sql_user = config['mysql']['user']
+    sql_pass = config['mysql']['password']
+    sql_db = config['mysql']['db']
+    sql_host = config['mysql']['host']
+    form_uid = config['kobo']['building_form_uid']
 
-sql = "SELECT submission_time from submissions order by submission_time desc limit 1";
-
-mycursor.execute(sql)
-latest_sub = mycursor.fetchone()[0]
-
-## Minus 1 day as a quick way to ensure we are not missing any submissions with to-the-second identical timestamps.
-date_to_filter = latest_sub - timedelta(days=1)
-filter_string = date_to_filter.strftime("%Y-%m-%dT%H:%M:%S")
-
-
-#####################################
-# GET data from Kobotools
-#####################################
-response = requests.get('%s/%s/data?query={"_submission_time":%%20{"$gt":"%s"}}' % (data_url, form_uid, filter_string),
-                        headers=headers,
-                        auth=(kobo_user,kobo_pass)
-                        );
-
-if response.status_code == 200:
-
-    buildings = response.json()['results']
-
-    with open(os.path.join('Data','xls','form_data.json'), '+w') as outfile:
-
-        json.dump(buildings, outfile)
-
-    print(len(buildings))
+    data_url = 'https://kobo.humanitarianresponse.info/api/v2/assets'
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
 
     #####################################
-    # Insert into Submissions table
+    # GET most recent submission_date from db
     #####################################
-    sql_submissions = "INSERT INTO submissions (`id`, `uuid`, `form_id`, `version`, `start`, `end`, `today`, `submission_time`, `submitted_by`, `submission`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE id=id"
+    mydb = mysql.connector.connect(
+                                       host=sql_host,
+                                       user=sql_user,
+                                       passwd=sql_pass,
+                                       database=sql_db
+                                       )
 
-    val_submissions = []
+    mycursor = mydb.cursor()
 
-    for building in buildings:
+    sql = "SELECT submission_time from submissions order by submission_time desc limit 1";
+
+    mycursor.execute(sql)
+    latest_sub = mycursor.fetchone()
+    filter_string = ""
+
+    if latest_sub is not None:
+        ## Minus 1 day as a quick way to ensure we are not missing any submissions with to-the-second identical timestamps.
+        latest_sub = latest_sub[0]
+        date_to_filter = latest_sub - timedelta(days=1)
+        date_string = date_to_filter.strftime("%Y-%m-%dT%H:%M:%S")
+        filter_string = '?query={"_submission_time":%%20{"$gt":"%s"}}' % (date_string)
+
+    #####################################
+    # GET data from Kobotools
+    #####################################
+    response = requests.get('%s/%s/data%s' % (data_url, form_uid, filter_string),
+                            headers=headers,
+                            auth=(kobo_user,kobo_pass)
+                            );
+
+    if response.status_code == 200:
+
+        buildings = response.json()['results']
 
 
-        entry = (
-                 building['_id'],
-                 building['_uuid'],
-                 building['_xform_id_string'],
-                 building['__version__'],
-                 to_mysql_date_time(building['start']),
-                 to_mysql_date_time(building['end']),
-                 building['today'],
-                 to_mysql_date_time(building['_submission_time']),
-                 building['_submitted_by'] or "not-set",
-                 json.dumps(building)
-                 )
-        val_submissions.append(entry)
+        print(len(buildings))
+
+        #####################################
+        # Insert into Submissions table
+        #####################################
+        sql_submissions = "INSERT INTO submissions (`id`, `uuid`, `form_id`, `version`, `start`, `end`, `today`, `submission_time`, `submitted_by`, `submission`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE id=id"
+
+        val_submissions = []
+
+        for building in buildings:
 
 
-    print(sql_submissions % val_submissions[0])
-    ##print(val_submissions)
+            entry = (
+                     building['_id'],
+                     building['_uuid'],
+                     building['_xform_id_string'],
+                     building['__version__'],
+                     to_mysql_date_time(building['start']),
+                     to_mysql_date_time(building['end']),
+                     building['today'],
+                     to_mysql_date_time(building['_submission_time']),
+                     building['_submitted_by'] or "not-set",
+                     json.dumps(building)
+                     )
+            val_submissions.append(entry)
 
-    mycursor.executemany(sql_submissions, val_submissions)
 
-    mydb.commit()
+        print(sql_submissions % val_submissions[0])
+        ##print(val_submissions)
 
-    # #####################################
-    # # Insert into Buildings table
-    # #####################################
-    sql_buildings = "INSERT INTO buildings (`id`, `cluster_id`, `structure_number`, `num_dwellings`, `latitude`, `longitude`, `altitude`, `precision`, `address`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE id=id"
+        mycursor.executemany(sql_submissions, val_submissions)
 
-    for building in buildings:
-
-        try:
-            gps = building['gps'].split()
-
-        except KeyError:
-            ## FOR DEMO ONLY
-            gps = [building['cheat/latitude'], building['cheat/longitude'], 0, 0]
-
-        building_entry = (
-                 building['_id'],
-                 building['cluster_id'],
-                 building['structure_number'],
-                 building['number_dwellings'],
-                 gps[0],
-                 gps[1],
-                 gps[2],
-                 gps[3],
-                 building['address'])
-
-        mycursor.execute(sql_buildings, (building_entry))
         mydb.commit()
-        print(mycursor.rowcount, "buildings were inserted")
+
         # #####################################
-        # # Generate Dwellings (Only if building was new)
+        # # Insert into Buildings table
         # #####################################
+        sql_buildings = "INSERT INTO buildings (`id`, `cluster_id`, `structure_number`, `num_dwellings`, `latitude`, `longitude`, `altitude`, `precision`, `address`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE id=id"
 
-        if(mycursor.rowcount > 0):
-            val_dwellings = []
+        for building in buildings:
 
-            for x in range(1, int(building['number_dwellings'])+1):
-                dwelling_entry = (
-                                  building['_id'],
-                                  x
-                                  )
+            try:
+                gps = building['gps'].split()
 
-                val_dwellings.append(dwelling_entry)
+            except KeyError:
+                ## FOR DEMO ONLY
+                gps = [building['cheat/latitude'], building['cheat/longitude'], 0, 0]
 
-            sql_dwellings = "INSERT INTO dwellings (`building_id`, `dwelling_number`) VALUES (%s, %s) ON DUPLICATE KEY UPDATE id=id"
+            building_entry = (
+                     building['_id'],
+                     building['cluster_id'],
+                     building['structure_number'],
+                     building['number_dwellings'],
+                     gps[0],
+                     gps[1],
+                     gps[2],
+                     gps[3],
+                     building['address'])
 
-            mycursor.executemany(sql_dwellings, val_dwellings)
+            mycursor.execute(sql_buildings, (building_entry))
             mydb.commit()
-            print(mycursor.rowcount, "dwellings were inserted")
+            print(mycursor.rowcount, "buildings were inserted")
+            # #####################################
+            # # Generate Dwellings (Only if building was new)
+            # #####################################
 
-        ## End Dwellings insert
-    ## End For buildings loop
-## End if response is success
-else:
-    print('Call to Kobotools returned status: %s' % response.status_code)
+            if(mycursor.rowcount > 0):
+                val_dwellings = []
+
+                for x in range(1, int(building['number_dwellings'])+1):
+                    dwelling_entry = (
+                                      building['_id'],
+                                      x
+                                      )
+
+                    val_dwellings.append(dwelling_entry)
+
+                sql_dwellings = "INSERT INTO dwellings (`building_id`, `dwelling_number`) VALUES (%s, %s) ON DUPLICATE KEY UPDATE id=id"
+
+                mycursor.executemany(sql_dwellings, val_dwellings)
+                mydb.commit()
+                print(mycursor.rowcount, "dwellings were inserted")
+
+            ## End Dwellings insert
+        ## End For buildings loop
+    ## End if response is success
+    else:
+        print('Call to Kobotools returned status: %s' % response.status_code)
+
+    return response.status_code
+
